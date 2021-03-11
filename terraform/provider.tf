79f86af44be0a90617da5af2fda1a2140b4ec262
duranlopes <duran750@hotmail.com>
2021-03-11 16:16:36 -0300

    change api version to v1

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = var.region
 }
 
-terraform {
-  backend "s3" {
-    bucket  = "terraform-state-duran"
-    key     = "terraform.tfstate"
-    region  = "us-east-1"
-    profile = "particular"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket  = "terraform-state-duran"
+#    key     = "terraform.tfstate"
+#    region  = "us-east-1"
+#    profile = "particular"
+#  }
+#}
