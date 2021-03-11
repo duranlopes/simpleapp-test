bcb3766e8f0542f356d87803de3f55960c7f5bc9
duranlopes <duran750@hotmail.com>
2021-03-10 21:19:58 -0300

    change max value

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,31 +12,30 @@ variable "kubernetes_version" {
 
 
 variable "desired_size" {
-  default = 2
+  default = 3
 }
 
 variable "min_size" {
-  default = 2
+  default = 3
 
 }
 
 variable "max_size" {
   default = 4
-
 }
 
-variable "auto_scale_cpu" {
-  default = {
-    scale_up_threshold  = 80
-    scale_up_period     = 60
-    scale_up_evaluation = 2
-    scale_up_cooldown   = 300
-    scale_up_add        = 2
-
-    scale_down_threshold  = 40
-    scale_down_period     = 120
-    scale_down_evaluation = 2
-    scale_down_cooldown   = 300
-    scale_down_remove     = -1
-  }
-}
+#variable "auto_scale_cpu" {
+#  default = {
+#    scale_up_threshold  = 80
+#    scale_up_period     = 60
+#    scale_up_evaluation = 2
+#    scale_up_cooldown   = 300
+#    scale_up_add        = 2
+#
+#    scale_down_threshold  = 40
+#    scale_down_period     = 120
+#    scale_down_evaluation = 2
+#    scale_down_cooldown   = 300
+#    scale_down_remove     = -1
+#  }
+#}
