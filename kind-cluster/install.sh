4e76a9adb58cd93e93882daf7cc3c1f99cbeca0c
duranlopes <duran750@hotmail.com>
2021-03-28 18:32:31 -0300

    add metric-server

--- kind-cluster/install.sh
+++ kind-cluster/install.sh
@@ -7,4 +7,4 @@ kind create cluster --config kind-config.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
-
+kubectl create -f metric-server.yaml
